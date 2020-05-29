28a8c25ef62593c64163347e0d15b33fc2c677e2
Devoughn Hall <devoughnhall@Devoughns-MacBook-Pro.local>
2020-05-29 15:00:57 -0400

    setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Link} from 'react-router-dom'
-
+import StreamCreate from './components/streams/StreamCreate'
+import StreamEdit from './components/streams/StreamEdit'
+import StreamDelete from './components/streams/StreamDelete'
+import StreamList from './components/streams/StreamList'
+import StreamShow from './components/streams/StreamShow'
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
